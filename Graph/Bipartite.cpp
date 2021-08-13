23581dd8a6201c383dcfb2852723e9c935b79573
s1270247 <69228985+s1270247@users.noreply.github.com>
2021-08-13 16:57:05 +0900

    Update Bipartite.cpp

--- Graph/Bipartite.cpp
+++ Graph/Bipartite.cpp
@@ -8,17 +8,15 @@ bool Check_Bipartite(vvi& g,vi& color,int v,int c){
   color.at(v)=c;
   repin(i,0,g.at(v).size()){
     if(color.at(g.at(v).at(i))==c) return false;
-    if(color.at(g.at(v).at(i))==0&&!Check_Bipartite(g,color,g.at(v).at(i),-c)) return false;
+    if(color.at(g.at(v).at(i))==0&&!dfs(g,color,g.at(v).at(i),-c)) return false;
   }
-
   return true;
 }
 
 /*
-無向グラフとして考える。
-グラフは1から始めるものとする。
+無向グラフを前提とする。
+グラフは1から始まるものとする。
 */
-
 int main(){
   cin.tie(0);
   ios::sync_with_stdio(0);
@@ -30,13 +28,14 @@ int main(){
       int a; cin>>a;
       if(a==-1) break;
       g.at(i).push_back(a-1); g.at(a-1).push_back(i);
-    }    
-    g.at(i).pop_back();
+    }
   }
 
   vi color(n,0);
-  repin(i,0,n) if(color.at(i)==0) if(!Check_Bipartite(g,color,i,1)) {cout<<"No"<<endl; return 0;}
+  repin(i,0,n) if(color.at(i)==0) if(!dfs(g,color,i,1)){
+      cout<<"No"<<endl; return 0;
+    }
+
   cout<<"Yes"<<endl;
-  
   return 0;
 }
